2466971a87ee05befc0c804e0233a045fdc545fe
jigul9977 <arashtrader9977@gmail.com>
2024-04-06 17:13:57 +0330

    Fetching User Information from an API and Displaying It in the User Panel

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,10 +1,49 @@
-import { getCookie } from "../../utils/cookie.js";
+import { authHandler } from "../../utils/authorization.js";
+import { getData } from "../../utils/httpReq.js";
 
-const init = () => {
-  const cookie = getCookie();
-  if (!cookie) {
-    location.assign("index.html");
-  }
+const userSection = document.getElementById("user-section");
+
+const showUsers = (users) => {
+  userSection.innerHTML = "";
+  users.forEach((user) => {
+    const {
+      id,
+      email,
+      name: { firstname, lastname },
+      username,
+      phone,
+      address: { city, street, zipcode },
+    } = user;
+    const userCardJSX = `
+    <div class="user-card">
+      <h3>${id}</h3>
+      <div>
+        <p><i class="fa-solid fa-user"></i> Name:</p>
+        <span>${firstname} ${lastname}</span>
+      </div>
+      <div>
+        <p><i class="fa-regular fa-id-badge"></i> Username:</p>
+        <span>${username}</span>
+      </div>
+      <div>
+        <p><i class="fa-solid fa-envelope"></i> Email:</p>
+        <span>${email}</span>
+      </div>
+      <div>
+        <p><i class="fa-solid fa-location-dot"></i> Address:</p>
+        <span>${city} - ${street} - ${zipcode}</span>
+      </div>
+    </div>`;
+    userSection.innerHTML += userCardJSX;
+  });
 };
+
+const init = async () => {
+  authHandler();
+  const users = await getData("users");
+  showUsers(users);
+};
+
+
 
 document.addEventListener("DOMContentLoaded", init);
